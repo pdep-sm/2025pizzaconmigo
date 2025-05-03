1b9435b5e0ba6ffe8a714ae0fcb9fbe5a70e5e5d
MatiFreyre <mfreyre@gmail.com>
2025-05-03 11:47:02 -0300

    Avance de clase del 3/5/2025

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,6 @@
 module Library where
 import PdePreludat
+import Data.Foldable (maximumBy)
 
 -- 1.a
 data Pizza = Pizza {
@@ -8,20 +9,9 @@ data Pizza = Pizza {
   calorias :: Number
 } deriving Show
 
-
-data P = P {
-  a :: String,
-  b :: B
-} deriving Show
-data B = B {
-  x :: Number,
-  y :: Number
-} deriving Show
-
-tt p = p{ b = (b p){x = 2} }
 -- 1.b
 grandeDeMuzza = Pizza ["salsa", "mozzarella", "orégano"] 8 350
-grandeDeJamonPalmitos = Pizza ["salsa", "mozzarella", "palmito", "jamon"] 8 500
+grandeDeJamonPalmitos = Pizza ["salsa", "mozzarella", "palmito", "jamón"] 8 500
 
 -- 2
 satisfaccion pizza 
@@ -44,7 +34,9 @@ valorPizza' (Pizza ings tam _) =
 -- 4.a
 nuevoIngrediente ingrediente pizza =
   (sumarCalorias (2 * length ingrediente) . agregarIngrediente ingrediente) pizza
-        
+nuevoIngrediente' ingrediente =
+  sumarCalorias (2 * length ingrediente) . agregarIngrediente ingrediente
+   
 agregarIngrediente ingrediente pizza = 
   pizza{ ingredientes = ingrediente : ingredientes pizza}
 -- pizza{ ingredientes = ingredientes pizza ++ [ingrediente] }
@@ -58,12 +50,12 @@ agrandar pizza = pizza{ tamanio = min 10 (tamanio pizza + 2) }
 -- 4.c
 mezcladita p1 p2 =
   sumarCalorias (calorias p1 / 2) p2{ ingredientes = ingredientes p2 ++ nuevosIngredientes }
-  where nuevosIngredientes = filter (not.(`elem` (ingredientes p2))) (ingredientes p1)
+  where nuevosIngredientes = filter (not.(`elem` ingredientes p2)) (ingredientes p1)
 -- where: Definición local
 
 mezcladita' p1 p2 =
   (sumarCalorias (calorias p1 / 2). agregarIngredientes nuevosIngredientes) p2
-  where nuevosIngredientes = filter (not.(`elem` (ingredientes p2))) (ingredientes p1)
+  where nuevosIngredientes = filter (not.(`elem` ingredientes p2)) (ingredientes p1)
 
 -- elem "palmito" ["muzza", "tomate"]
 -- "palmito" `elem` ["muzza", "tomate"]
@@ -75,31 +67,70 @@ agregarIngredientes ingredientes pizza =
 agregarIngredientes' ingredientes pizza =
   foldl (flip agregarIngrediente) pizza ingredientes
 
+-- 5
+-- Pedidos! Ahora si llegaron las pip-sas
+type Pedido = [Pizza]
+
+satisfaccionPedido :: Pedido -> Number
+satisfaccionPedido = sum . map satisfaccion
+
+-- 6
+type Pizzeria = Pedido -> Pedido
+pizzeriaLosHijosDePato, pizzeriaElResumen :: Pizzeria
+pizzeriaEspecial :: Pizza -> Pizzeria
+pizzeriaGourmet :: Number -> Pizzeria
+
+--6.a
+pizzeriaLosHijosDePato = map (agregarIngrediente "palmito")
+
+--6.b
+pizzeriaElResumen pedido = zipWith mezcladita' pedido (tail pedido)
+
+--6.c
+pizzeriaEspecial predilecta = map (mezcladita' predilecta)
+anchoasBasica :: Pizza
+anchoasBasica = Pizza {
+  ingredientes = ["salsa", "anchoas"],
+  calorias = 270,
+  tamanio = 8
+}
+pizzeriaPescadito :: Pizzeria
+pizzeriaPescadito = pizzeriaEspecial anchoasBasica
+
+--6.d
+pizzeriaGourmet nivelExquisitez = map agrandar . filter ((>nivelExquisitez).satisfaccion)
+pizzeriaLaJauja :: Pizzeria
+pizzeriaLaJauja = pizzeriaGourmet 399
+
 -- foldl' _ s [] = s
 -- foldl' f s (x:xs) = foldl' f (f s x) xs
 
 -- foldr' _ s [] = s
 -- foldr' f s (x:xs) = f x (foldr' f s xs)
 
--- Pedidos! Ahora si llegaron las pip-sas
--- 5
+-- foldl1' f (x:xs) = foldl' f x xs
+-- foldr1' f lista  = foldr' f (last lista) (init lista)
 
 
--- 6
---6.a
---pizzeriaLosHijosDePato
+--7.a
+sonDignasDeCalleCorrientes :: Pedido -> [Pizzeria] -> [Pizzeria]
+sonDignasDeCalleCorrientes pedido pizzerias = filter (esDignaDeCalleCorrientes pedido) pizzerias
 
---6.b
---pizzeriaElResumen
+esDignaDeCalleCorrientes :: Pedido -> Pizzeria -> Bool
+esDignaDeCalleCorrientes pedido pizzeria =
+  satisfaccionPedido pedido < (satisfaccionPedido . pizzeria) pedido
 
---6.c
---pizzeriaEspecial
---pizzeriaPescadito
+--7.b
+mejorPizzeria :: Pedido -> [Pizzeria] -> Pizzeria
+mejorPizzeria pedido pizzerias = 
+  foldl1 (mayorSatisfaccion pedido) pizzerias
 
---6.d
---pizzeriaGourmet
---pizzeriaLaJauja
+mayorSatisfaccion :: Pedido -> Pizzeria -> Pizzeria -> Pizzeria
+mayorSatisfaccion pedido pizzeria1 pizzeria2  
+  | valor pizzeria1 > valor pizzeria2 = pizzeria1
+  | otherwise = pizzeria2
+  where valor pizzeria = satisfaccionPedido . pizzeria $ pedido
 
-
---7
--- sonDignasDeCalleCorrientes
+-- 9
+laPizzeriaPredilecta = foldl (.) id
+laPizzeriaPredilecta' = foldl1 (.)
